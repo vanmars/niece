75318b1afbeeaca7c7d750972677a4377a2e9896
Vanessa Stewart <vstewart1213@gmail.com>
2020-08-11 10:51:39 -0700

    Add light and dark background buttons with jQuery
    Co-authored-by: Alex Goodwin <hagoodwi@gmail.com> and Joseph Karnafel <josephkarnafel@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$("document").ready(function(){
+  $("button#light").click(function () {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
+  $("button#dark").click(function () {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
+
+});
